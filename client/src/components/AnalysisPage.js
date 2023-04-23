299d8307328e67c441ca3af37d38b41f5da414b1
aggarnav <aggarnav@gmail.com>
2023-04-23 15:30:19 -0400

    Added complete team-circuit preferences

--- client/src/components/AnalysisPage.js
+++ client/src/components/AnalysisPage.js
@@ -1,94 +1,59 @@
 import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import Select from 'react-select';
+import countries from '../data/teams.json';
+import circuits from '../data/circuits.json';
 
 
-const AnalysisPage = props => {
-    const finalSpaceCharacters = [
-        {
-          id: 'gary',
-          name: 'Gary Goodspeed'
-        },
-        {
-          id: 'cato',
-          name: 'Little Cato'
-        },
-        {
-          id: 'kvn',
-          name: 'KVN'
-        },
-        {
-          id: 'mooncake',
-          name: 'Mooncake'
-        },
-        {
-          id: 'quinn',
-          name: 'Quinn Ergon'
-        }
-      ]
-    
-      const [characters, updateCharacters] = useState(finalSpaceCharacters);
-    
-      function handleOnDragEnd(result) {
-        if (!result.destination) return;
-    
-        const items = Array.from(characters);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-    
-        updateCharacters(items);
-      }  
+
+const AnalysisPage = props => {  
+    const [characters, updateCharacters] = useState(circuits);
+    const [label, updateLabel] = useState(0);
+  
+    function handleOnDragEnd(result) {
+      if (!result.destination) return;
+  
+      const total = Array.from(characters)
+      const items = Array.from(characters[label]);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      
+      total[label] = items
+      updateCharacters(total);
+    }
+          
+    function handleChange(e){
+      updateLabel(e.value)
+      }
+           
     
     return (
-          <table>
-            <tr><td>
-              <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="characters">
-                  {(provided) => (
-                    <ol className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                      {characters.map(({id, name}, index) => {
-                        return (
-                          <Draggable key={id} draggableId={id} index={index}>
-                            {(provided) => (
-                              <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                <p>
-                                  { name }
-                                </p>
-                              </li>
-                            )}
-                          </Draggable>
-                        );
-                      })}
-                      {provided.placeholder}
-                    </ol>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </td>
-            <td>
-              <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="characters">
-                  {(provided) => (
-                    <ol className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                      {characters.map(({id, name}, index) => {
-                        return (
-                          <Draggable key={id} draggableId={id} index={index}>
-                            {(provided) => (
-                              <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                <p>
-                                  { name }
-                                </p>
-                              </li>
-                            )}
-                          </Draggable>
-                        );
-                      })}
-                      {provided.placeholder}
-                    </ol>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </td></tr>
-          </table>
+      <div className="container">
+            <Select options={countries} onChange={handleChange} defaultValue={countries[label]}/>
+            <br></br>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+              <Droppable droppableId="characters">
+                {(provided) => (
+                  <ol className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                    {characters[label].map((name, index) => {
+                      return (
+                        <Draggable key={name} draggableId={name} index={index}>
+                          {(provided) => (
+                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <p>
+                                { name }
+                              </p>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </ol>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
       );    
 };
 
